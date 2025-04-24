646b82d6825e3f59d0df2d0af5fb40981b10a7cd
simchafridman <simcha.fridman2@gmail.com>
2025-04-24 22:12:00 +0300

    step 2 add task table and delete task

--- resources/js/pages/Tasks/Index.tsx
+++ resources/js/pages/Tasks/Index.tsx
@@ -1,12 +1,55 @@
 import AppLayout from '@/layouts/app-layout';
-import { Head } from '@inertiajs/react';
+import { Head, router } from '@inertiajs/react';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { Button, buttonVariants } from '@/components/ui/button';
+import { Task } from '@/types';
+import { toast } from 'sonner';
 
-export default function Index() {
+export default function Index({ tasks }: { tasks: Task[] }) {
+
+    const deleteTask = (id: number) => {
+        if (confirm('Are you sure?')) {
+            router.delete(route('tasks.destroy', { id }));
+            toast.success('Task deleted successfully');
+        }
+    };
+
     return (
         <AppLayout>
             <Head title="Tasks List" />
             <div>
-                The list will be here.
+                <Table className={'mt-4'}>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>Task</TableHead>
+                            <TableHead className="w-[100px]">Status</TableHead>
+                            <TableHead className="w-[150px] text-right">Actions</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {tasks.map((task) => (
+                            <TableRow key={task.id}>
+                                <TableCell>{task.name}</TableCell>
+                                <TableCell className={task.is_completed ? 'text-green-600' : 'text-red-700'}>
+                                    {task.is_completed ? 'Completed' : 'In Progress'}
+                                </TableCell>
+                                <TableCell className="flex flex-row gap-x-2 text-right">
+                                    <Button variant={'destructive'} className={'cursor-pointer'} onClick={() => deleteTask(task.id)}>
+                                        Delete
+                                    </Button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+
             </div>
         </AppLayout>
     );
